19666a3a0204e821bea29ca1317ea36df468eddf
Thomas Rogg <thomas@neonious.com>
2019-10-16 01:15:48 +0200

    added timers module, used by mysql module

---
+++ lib_js/timers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Re-export process as a native module
+module.exports = {
+    setTimeout,
+    setInterval,
+    setImmediate,
+    clearTimeout,
+    clearInterval,
+    clearImmediate
+};
