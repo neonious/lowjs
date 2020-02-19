6e76e2d3a9a23b1f6fcebc55326ca64c7a4c0adb
Thomas Rogg <t.rogg@neonious.com>
2020-02-19 02:35:52 +0100

    fixed #94

--- lib_js/internal/http.js
+++ lib_js/internal/http.js
@@ -120,7 +120,7 @@ const STATUS_CODES = {
     511: 'Network Authentication Required' // RFC 6585
 };
 
-const discard2Headers = { 'age': 1, 'authorization': 1, 'content-length': 1, 'content-type': 1, 'etag': 1, 'expires': 1, 'from': 1, 'host': 1, 'if-modified-since': 1, 'if-unmodified-since': 1, 'last-modified': 1, 'location': 1, 'max-forwards': 1, 'proxy-authorization': 1, 'referer': 1, 'retry-after': 1, 'user-agent': 1 };
+const discard2headers = { 'age': 1, 'authorization': 1, 'content-length': 1, 'content-type': 1, 'etag': 1, 'expires': 1, 'from': 1, 'host': 1, 'if-modified-since': 1, 'if-unmodified-since': 1, 'last-modified': 1, 'location': 1, 'max-forwards': 1, 'proxy-authorization': 1, 'referer': 1, 'retry-after': 1, 'user-agent': 1 };
 
 class IncomingMessage extends stream.Readable {
     // event "aborted"
